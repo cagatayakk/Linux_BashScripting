c3046d1a1d567ab220bfe1f35bf438339e8c2ca7
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-09-16 09:03:36 +0100

    Add files via upload

---
+++ comment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "hello"
+# date
+pwd # This is an inline comment
+# ls
+
+# here it is a heredoc and it's syntax
+cat << EOF
+Welcome to the Linux Lessons.
+This lesson is about the shell scripting
+EOF
+
+
+#  If the HereDoc block is not redirected to a command, it can serve as a multiline comments placeholder.
+<< multiline-comment
+pwd
+ls
+Everything inside the
+HereDoc body is
+a multiline comment
+multiline-comment
+
