7b386673b46c589a1e91170e15ab43a4a88885c5
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-10-06 19:21:20 +0200

    Update script.sh

--- Project-101/First_Part/script.sh
+++ Project-101/First_Part/script.sh
@@ -15,3 +15,8 @@ do
 done
 
 cat $FILE | grep serdar | grep Terminate | grep -Eo "i-[a-zA-Z0-9]{17}" | sort | uniq > /tmp/result.txt 19  20 echo "Your result is ready under the /tmp/result.txt file"
+
+
+
+#other way
+#cat event_history.csv | grep -i serdar | grep -i terminateinstances | cut -d',' -f 10 | cut -d':' -f 2 | sed 's/""//' | sed 's/""}//' | sed 's/]"//' | tee result.txt
