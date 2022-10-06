a2cf08ba2e69d298c87924253bb031da3c179612
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-10-06 19:52:12 +0200

    Update user_passwd.sh

--- Project-101/Second_Part/user_passwd.sh
+++ Project-101/Second_Part/user_passwd.sh
@@ -1,53 +1,57 @@
 #!/bin/bash
-#
-# This script creates a new user on the local system.
-# You will be prompted to enter the username (login), the person name, and a password.
-# The username, password, and host for the account will be displayed.
 
-# Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" -ne 0 ]]
-then
-   echo 'Please run this script with sudo or as root.'
-   exit 1
+#This script creates a new user on the local system.
+#You will be prompted to enter username (login), the person name and a password.
+#The username, password and the host for the account will be displayed.
+
+#Make sure the script is being executed with superuser privileges.
+
+if [[ $UID != 0 ]]; then
+    echo "You don't have superuser privileges. Make sure to run script as root user."
+    exit 1
 fi
 
-# Get the username (login).
-read -p 'Enter the username to create: ' USER_NAME
+#Get the username(login) 
 
-# Get the real name (contents for the description field).
-read -p 'Enter the name of the person or application that will be using this account: ' COMMENT
+read -p "Enter your login name: " login_name
 
-# Get the password.
-read -sp 'Enter the password to use for the account: ' PASSWORD
+#Get the real name (contents for the description field)
 
-# Create the account.
-useradd -c "${COMMENT}" -m ${USER_NAME} 2> /dev/null 
+read -p "Enter your full name: " full_name
 
-# Check to see if the useradd command succeeded.
-# We don't want to tell the user that an account was created when it hasn't been.
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'This username is already exit. Please select different username '
-  exit 1
+#Get the password
+
+read -sp "Enter your password: " password
+
+#Create the account
+
+useradd -m ${login_name} -c "${full_name}" 
+
+#Check to see if the useradd command succeeded
+#We don't want to tell the user that an account was created when it hasn't been
+
+if [[ ${?} != 0 ]]; then
+    echo "This username is already exist. Please select different username."
+    exit 1
 fi
 
-# Set the password.
-echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+#Set the password
 
-# Check to see if the passwd command succeeded.
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'The password for the account could not be set.'
-  exit 1
+echo ${password} | passwd --stdin ${login_name}
+
+#Check to see if the passwd command succeeded
+
+if [[ ${?} != 0 ]]; then
+    echo "This password couldn't set."
+    exit 1
 fi
 
-# Force password change on first login.
-passwd -e ${USER_NAME}
+#Force password change on first login
 
-# Display the username, password, and the host where the user was created.
-echo
-echo 'username:'
-echo "${USER_NAME}"
-echo
-echo 'password:'
-echo "${PASSWORD}"
+passwd -e ${login_name}
+
+#Display the username, password and the the host where the user was created
+
+echo ${login_name}
+echo ${full_name}
+echo ${password}
