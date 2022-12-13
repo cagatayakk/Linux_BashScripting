7022e045a542d0efef22900fb91c61eb4dae4015
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-12-13 14:35:02 +0100

    Update stress-tool.sh

--- stress-tool.sh
+++ stress-tool.sh
@@ -1,3 +1,9 @@
 sudo amazon-linux-extras install epel -y
 sudo yum install -y stress
 stress --cpu 80 --timeout 20000   
+
+
+
+# for Container
+# kubectl run -it --rm load-generator --image=busybox /bin/sh  
+# -> while true; do wget -q -O- http://<puplic ip>:<port number of NordePord-service>; done 
